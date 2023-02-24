c80b99985076846daefb9c730c86a5869947fe6b
thay279931 <111098654+thay279931@users.noreply.github.com>
2023-02-24 09:53:48 +0800

    key

--- src/keys.js
+++ src/keys.js
@@ -1,6 +1,5 @@
 const keys = {
-  gmap: 'AIzaSyCd1sBbFNoxp5IzvsQINqW_lXEJjpEQYSA',
+  gmap: 'AIzaSyAS_Z3KF3hoMNA4PIGuBHtU0YEDGNCc6F0',
   // gmap: 'AIzaSyCd1sBbFNoxp5IzvsQINqW_lXEJjpEQYSA4',
-
 };
 export default keys;
